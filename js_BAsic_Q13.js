86c4e59c7f0aa5ba63d22c1c3d168478c33d1c77
Ajit1484 <ajitdeshmukh1997@gmail.com>
2025-01-02 00:19:18 +0530

    Add files via upload

---
+++ js_BAsic_Q13.js
@@ -0,0 +1,14 @@
+
+let numValue = 42;
+let stringValue = "42";
+
+let res = numValue + stringValue;
+
+console.log(res);
+
+if(res > 100){
+  console.log("the result is greater than 100");
+}
+else {
+  console.log("the result is less than or equal to 100");
+}

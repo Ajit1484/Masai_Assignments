cc4cc633a279a98a7b6e308a323dcd32b5252b84
Ajit1484 <ajitdeshmukh1997@gmail.com>
2025-01-02 18:11:06 +0530

    Add files via upload

---
+++ problem1_AreaCalculator.js
@@ -0,0 +1,18 @@
+
+
+let base = -2;
+let height = 5;
+
+let area_Triangle = (base * height)/2;
+
+console.log("Area of triangle is :"+area_Triangle);
+
+if(base == 0 || height == 0){
+  console.log("zero");
+}
+
+else if(base || height < 0){
+  console.log("Invalid number, base and height should be positive");
+}
+
+

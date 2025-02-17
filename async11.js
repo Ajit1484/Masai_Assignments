d552d91299d35af980e6ca48d69e4e052c7ab757
Ajit1484 <ajitdeshmukh1997@gmail.com>
2025-02-17 23:13:47 +0530

    Add files via upload

---
+++ async11.js
@@ -0,0 +1,13 @@
+function task1(){
+  console.log("1st completed");
+}
+function task2(){
+  console.log("2nd completed");
+}
+function task3(){
+  console.log("3rd completed");
+}
+
+task1();
+task2();
+task3();

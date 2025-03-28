5bd3d1c44d7128b74c6583955626b3e44f406e95
Ajit1484 <ajitdeshmukh1997@gmail.com>
2025-03-29 01:36:03 +0530

    Add files via upload

---
+++ day4_question4.js
@@ -0,0 +1,18 @@
+function createFunctionList() {
+
+  let functions = [];
+  
+  for (let i = 0; i < 5; i++) {
+  
+  functions.push(function () {
+  
+  console.log("Index:", i);
+  
+  });
+  
+  }
+  
+  return functions; }
+
+  let result = createFunctionList()
+  console.log(result)

db12f2f8d0de697559851c5a760ab0a0d046b20c
Ajit1484 <ajitdeshmukh1997@gmail.com>
2025-04-06 23:56:04 +0530

    Add files via upload

---
+++ sprint_2coding22.js
@@ -0,0 +1,18 @@
+
+//=====[[[[[[[ My camera is not working so i used this ]]]]]]]==================================
+
+
+
+
+//1) what is promise and why do we need it.
+
+// promise is a concept in a JS which gives us to handle a async function. promice has three states (resolve,reject,pending). and we can handle this states by try and catch or by async await.JS
+
+// before promises we use callback function. callback functions are difficult to debug , difficult to Read. and make a problem as a calback hell. callback hell also called as pyaramid doom.JS
+
+// Promises help us to solve code easy, cleaner, more readable.
+
+
+// 2) what is callback hell??
+
+// Callback hell is when you have many nested callbacks, making your code hard to read.

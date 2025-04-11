c9efb37349e3a277c609a7237d4da8f3fab6e870
Ajit1484 <ajitdeshmukh1997@gmail.com>
2025-04-12 00:00:25 +0530

    Add files via upload

---
+++ factoryfun04.js
@@ -0,0 +1,8 @@
+function createBook(title, author) {
+  let obj = {};
+    this.title=title
+    this.author=author
+    printInfo=function() {
+      console.log(`Title: ${this.title}, Author: ${this.author}`);
+    }
+  };
